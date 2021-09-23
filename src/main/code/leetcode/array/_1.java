434fd723b7031c4209170e3f3ad987118142cb2e
xialongyun <xia_longyun@163.com>
2021-09-23 10:03:44 +0800

    update

---
+++ src/main/code/leetcode/array/_1.java
@@ -0,0 +1,43 @@
+package main.code.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int firstNum = 0;
+        int secondNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    firstNum = i;
+                    secondNum = j;
+                }
+            }
+        }
+        return new int[]{firstNum, secondNum};
+    }
+
+}
