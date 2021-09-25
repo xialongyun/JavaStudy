e513291fa9d681bfa1b2038291ec892ccc239ec6
xialongyun <xia_longyun@163.com>
2021-09-25 23:06:39 +0800

    update-from-lab

---
+++ src/main/code/leetcode/array/_53.java
@@ -0,0 +1,13 @@
+package main.code.leetcode.array;
+
+public class _53 {
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+
+    }
+}
