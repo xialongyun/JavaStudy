82dcd1df3fdbd48b47f42a7ff646c3b364f6ab5c
xialongyun <xia_longyun@163.com>
2021-09-28 15:32:11 +0800

    update-company

---
+++ src/main/code/leetcode/list/_83.java
@@ -0,0 +1,27 @@
+package main.code.leetcode.list;
+
+public class _83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode curr = head;
+        while(curr.next != null) {
+            if(curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+
+        }
+        return head;
+
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
