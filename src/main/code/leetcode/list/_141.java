cdbd5d62fa1a25de3def8cbf129e2946b55607e7
xialongyun <xia_longyun@163.com>
2021-09-28 14:09:33 +0800

    update-company

--- src/main/code/leetcode/list/_141.java
+++ src/main/code/leetcode/list/_141.java
@@ -4,7 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class _141 {
-    public boolean hasCycle(ListNode head) {
+    /**
+     * 哈希表
+    * */
+    public boolean hasCycle1(ListNode head) {
         Set<ListNode> set = new HashSet<ListNode>();
         while(head != null) {
             if(!set.add(head)) {
@@ -13,6 +16,25 @@ public class _141 {
             head = head.next;
         }
         return false;
+    }
+
+    /**
+     * 双指针(快慢指针)
+     * */
+    public boolean hasCycle2(ListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+        ListNode low = head;
+        ListNode fast = head.next;
+        while(low != fast) {
+            if(fast == null || fast.next == null) {
+                return false;
+            }
+            low = low.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 
     public static class ListNode {
