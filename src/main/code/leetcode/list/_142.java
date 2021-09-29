b1944781529cbf25276ebf698c508df5fdb59009
xialongyun <xia_longyun@163.com>
2021-09-29 15:09:33 +0800

    update-company

---
+++ src/main/code/leetcode/list/_142.java
@@ -0,0 +1,27 @@
+package main.code.leetcode.list;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = head;
+        Set<ListNode> set = new HashSet<ListNode>();
+        while(pos != null) {
+            if(set.contains(pos)) {
+                return pos;
+            }
+            set.add(pos);
+            pos = pos.next;
+        }
+        return null;
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
