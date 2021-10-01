0627fab541addcc12a390966ae4abd34a1e3add0
xialongyun <xia_longyun@163.com>
2021-10-01 18:58:30 +0800

    update-from-lab

--- src/main/code/leetcode/tree/_94_inOrder.java
+++ src/main/code/leetcode/tree/_94_inOrder.java
@@ -2,6 +2,7 @@ package main.code.leetcode.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class _94_inOrder {
     /**
@@ -20,6 +21,26 @@ public class _94_inOrder {
         inorder(root.left, res);
         res.add(root.val);
         inorder(root.right, res);
+    }
+    /**
+     * 非递归——中序遍历
+     * */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root != null) {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            while(!stack.isEmpty() || root != null) {
+                if(root != null) {
+                    stack.push(root);
+                    root = root.left;
+                } else {
+                    root = stack.pop();
+                    res.add(root.val);
+                    root = root.right;
+                }
+            }
+        }
+        return res;
     }
 
     public class TreeNode {
