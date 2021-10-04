7350a4d2c2cfdd8688f423e997d0bd5ab449782a
xialongyun <xia_longyun@163.com>
2021-10-04 22:28:01 +0800

    update-from-lab

---
+++ src/main/code/leetcode/tree/_235.java
@@ -0,0 +1,13 @@
+package main.code.leetcode.tree;
+
+public class _235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (q.val < root.val && p.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
