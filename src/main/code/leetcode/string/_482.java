304ccd5722d9810afdb07cd3e35486eff59ff668
xialongyun <xia_longyun@163.com>
2021-10-04 17:47:20 +0800

    update-from-lab

--- src/main/code/leetcode/string/_482.java
+++ src/main/code/leetcode/string/_482.java
@@ -2,23 +2,21 @@ package main.code.leetcode.string;
 
 public class _482 {
     public String licenseKeyFormatting(String s, int k) {
-        public String licenseKeyFormatting(String s, int k) {
-            StringBuffer str = new StringBuffer();
-            s = s.toUpperCase();
-            int count = 0;
-            for (int i = s.length() - 1; i >= 0; i--) {
-                if (s.charAt(i) != '-') {
-                    str.insert(0, s.charAt(i));
-                    count++;
-                    if (count % k == 0) {
-                        str.insert(0, '-');
-                    }
+        StringBuffer str = new StringBuffer();
+        s = s.toUpperCase();
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '-') {
+                str.insert(0, s.charAt(i));
+                count++;
+                if (count % k == 0) {
+                    str.insert(0, '-');
                 }
             }
-            if(str.length() != 0 && str.charAt(0) == '-') {
-                str.delete(0, 1);
-            }
-            return str.toString();
         }
+        if(str.length() != 0 && str.charAt(0) == '-') {
+            str.delete(0, 1);
+        }
+        return str.toString();
     }
 }
