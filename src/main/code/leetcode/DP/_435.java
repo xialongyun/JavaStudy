541bce04d09c988eb8f8c76d25730a5e95f5f4de
xialongyun <xia_longyun@163.com>
2021-10-08 23:09:26 +0800

    update-from-lab

---
+++ src/main/code/leetcode/DP/_435.java
@@ -0,0 +1,30 @@
+package main.code.leetcode.DP;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class _435 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 1) {
+            return 0;
+        }
+        List<int[]> list = new ArrayList<>();
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= right) {
+                ans++;
+                right = intervals[i][1];
+            }
+        }
+        return intervals.length - ans;
+    }
+}
