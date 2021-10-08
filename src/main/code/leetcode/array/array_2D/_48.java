c1b1f74659cadca5f34a2f42d2ebfacbbe14dcdb
xialongyun <xia_longyun@163.com>
2021-10-08 10:15:54 +0800

    update-company

---
+++ src/main/code/leetcode/array/array_2D/_48.java
@@ -0,0 +1,26 @@
+package main.code.leetcode.array.array_2D;
+
+public class _48 {
+    public void rotate(int[][] matrix) {
+        T(matrix);
+        S(matrix);
+    }
+    public void T(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    public void S(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
+            }
+        }
+    }
+}
